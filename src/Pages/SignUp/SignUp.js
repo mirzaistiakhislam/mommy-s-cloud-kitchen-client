ec613c627cfb4932dd36846b6ed69218e65b6afd
mirzaistiakhislam <mirza15-9603@diu.edu.bd>
2022-11-20 06:57:26 +0600

    add login and register

---
+++ src/Pages/SignUp/SignUp.js
@@ -0,0 +1,61 @@
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
+
+const SignUp = () => {
+
+    const { createUser } = useContext(AuthContext);
+
+    const handleSignUp = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        createUser(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+            })
+            .catch(error => console.error(error));
+    }
+
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content grid md:grid-cols-2 flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold text-center">Sign Up!</h1>
+                    <p className="py-6">For your better experience with Cloud Kitchen</p>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 pb-10">
+                    <form onSubmit={handleSignUp} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name='name' placeholder="Enter your name" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" name='email' placeholder="Enter your email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control mt-6">
+                            <input type="submit" className="btn btn-primary" value="Sign Up" />
+                        </div>
+                    </form>
+                    <p className='text-center'>Already have an account? <Link className='text-blue-600 font-bold' to="/login">Login</Link></p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SignUp;
